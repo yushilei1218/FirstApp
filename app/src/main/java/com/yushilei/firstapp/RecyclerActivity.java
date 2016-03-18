d30327e5d599b23c890870d7c11857049395d592
yushilei <yushilei.1989@163.com>
2016-03-18 16:26:20 +0800

    first commit

--- app/src/main/java/com/yushilei/firstapp/RecyclerActivity.java
+++ app/src/main/java/com/yushilei/firstapp/RecyclerActivity.java
@@ -2,12 +2,26 @@ package com.yushilei.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(i + "条");
+        }
+        adapter.addAll(list);
     }
 }
---
+++ app/src/main/java/com/yushilei/firstapp/RecyclerAdapter.java
@@ -0,0 +1,58 @@
+package com.yushilei.firstapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jh on 2016/3/18.
+ */
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+
+    private List<String> data;
+
+    public RecyclerAdapter() {
+        this.data = new ArrayList<>();
+    }
+
+    public void addAll(List<String> list) {
+        if (list != null) {
+            int size = data.size();
+            int size1 = list.size();
+            data.addAll(list);
+            notifyItemRangeInserted(size, size1);
+        }
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.textView.setText(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.item_text);
+        }
+    }
+}
