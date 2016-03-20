93de0b54514962dd90e73a47706b79d704484c12
yushilei <yushilei.1989@163.com>
2016-03-20 11:52:17 +0800

    third commit

--- app/src/main/java/com/yushilei/firstapp/util/NetWorkUtil.java
+++ app/src/main/java/com/yushilei/firstapp/util/NetWorkUtil.java
@@ -1,10 +1,20 @@
 package com.yushilei.firstapp.util;
 
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
 /**
  * Created by jh on 2016/3/20.
  */
 public class NetWorkUtil {
-    public interface Service{
+    public interface Service {
 
+    }
+
+    private static Service service;
+
+    static {
+        service = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl("http://www.baidu.com")
+                .build().create(Service.class);
     }
 }
